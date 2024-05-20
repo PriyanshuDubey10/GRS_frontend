53d091f7dc49bfd037619e6f4559d2d4b1d698c7
rockers990 <83651376+rockers990@users.noreply.github.com>
2024-05-20 22:41:33 +0530

    Update ad_register.jsx

--- src/component/ad_register.jsx
+++ src/component/ad_register.jsx
@@ -11,7 +11,7 @@ const Adminregister = () => {
     const [password, setPassword] = useState('');
     const { setUser } = useAuth();
 
-    const BASE_URL = 'https://grs-backend-1.onrender.com';
+    const BASE_URL = 'https://grs-backend-2.onrender.com';
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
